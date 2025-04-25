86925c3463ac1db5dcf092824aee2be9bac2d299
Md Abu Shayed <shayedhasandhrubo@gmail.com>
2025-04-25 22:31:16 +0600

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:custom_splash_screen/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      initialRoute: '/',
+      routes: {
+        '/':(context)=> SplashScreen(),
+        '/home':(context)=> HomeScreen(),
+      },
+    );
+  }
+}
+
---
+++ lib/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState(){
+    super.initState();
+    Future.delayed(Duration(seconds: 3),(){
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.book, size: 100, color: Colors.white,),
+            Text('Read Book Daily', style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),)
+          ],
+        ),
+      ),
+    );
+  }
+}
